21eff390a8d1515cdf537f55598ee7e921214256
Lucas Lemos <llcf@ic.ufal.br>
2023-12-24 19:22:06 -0300

    fix: eslint/prettier fixes

--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -1,17 +1,18 @@
-import * as React from "react"
+import * as React from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa'
+
 import {
   useColorMode,
   useColorModeValue,
   IconButton,
   IconButtonProps,
-} from "@chakra-ui/react"
-import { FaMoon, FaSun } from "react-icons/fa"
+} from '@chakra-ui/react'
 
-type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
+type ColorModeSwitcherProps = Omit<IconButtonProps, 'aria-label'>
 
 export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
   const { toggleColorMode } = useColorMode()
-  const text = useColorModeValue("dark", "light")
+  const text = useColorModeValue('dark', 'light')
   const SwitchIcon = useColorModeValue(FaMoon, FaSun)
 
   return (
