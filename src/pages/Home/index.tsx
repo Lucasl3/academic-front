68fe51b736afb4763697efd8fe6ce521ccabd767
Lucas Lemos <llcf@ic.ufal.br>
2023-12-27 16:45:38 -0300

    feat: init page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,9 @@
-import * as React from 'react'
+import React from 'react'
 
-import { Box, Text, Link, VStack, Code, Grid } from '@chakra-ui/react'
-
-import { ColorModeSwitcher } from '@/ColorModeSwitcher'
+import { Box } from '@chakra-ui/react'
 
 const Home = () => {
-  return (
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Router
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  )
+  return <Box>Teste</Box>
 }
 
 export default Home
