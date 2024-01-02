69763e02dd0380a8d95aa31fbf5f41e90b239b30
Lucas Lemos <llcf@ic.ufal.br>
2024-01-02 10:55:45 -0300

    feat: added links

--- src/data/template/index.ts
+++ src/data/template/index.ts
@@ -12,9 +12,9 @@ import { TTemplateSidebarProps } from './types'
 export const sidebarProps = {
   linkItems: [
     { name: 'Home', icon: FiHome, to: '/' },
-    { name: 'Trending', icon: FiTrendingUp, to: '/' },
-    { name: 'Explore', icon: FiCompass, to: '/' },
-    { name: 'Favourites', icon: FiStar, to: '/' },
-    { name: 'Settings', icon: FiSettings, to: '/' },
+    { name: 'Trending', icon: FiTrendingUp, to: '/teste1' },
+    { name: 'Explore', icon: FiCompass, to: '/teste2' },
+    { name: 'Favourites', icon: FiStar, to: '/teste3' },
+    { name: 'Settings', icon: FiSettings, to: '/teste4' },
   ],
 } as TTemplateSidebarProps
