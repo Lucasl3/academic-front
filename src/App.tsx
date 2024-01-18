2ec60632b29a8d4ac758e51fcac8eac82e205ef0
Lucas Lemos <llcf@ic.ufal.br>
2024-01-18 14:44:59 -0300

    feat: protect dashboard routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import TemplatePage from '@/common/Templates/TemplatePage'
 import SidebarContextProvider from '@/contexts/SidebarContext'
+import PrivateRoutes from '@/guard/auth.guard'
 import Login from '@/pages/Auth/Login'
 import Register from '@/pages/Auth/Register'
 import Home from '@/pages/Home'
+
 class App extends React.Component {
   render() {
     return (
       <SidebarContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<TemplatePage />}>
-              <Route path="teste1" element={<Home />} />
-              <Route index element={<h1>Teste 1</h1>} />
-              <Route path="teste2" element={<h1>Teste 2</h1>} />
-              <Route path="teste3" element={<h1>Teste 3</h1>} />
-              <Route path="teste4" element={<h1>Teste 4</h1>} />
+            <Route element={<PrivateRoutes />}>
+              <Route path="/dashboard" element={<TemplatePage />}>
+                <Route path="teste1" element={<Home />} />
+                <Route index element={<h1>Teste 1</h1>} />
+                <Route path="teste2" element={<h1>Teste 2</h1>} />
+                <Route path="teste3" element={<h1>Teste 3</h1>} />
+                <Route path="teste4" element={<h1>Teste 4</h1>} />
+              </Route>
             </Route>
             <Route path="/auth">
               <Route path="login" element={<Login />} />
               <Route path="cadastrar" element={<Register />} />
             </Route>
-            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="*" element={<Navigate to="/auth/login" />} />
           </Routes>
         </BrowserRouter>
       </SidebarContextProvider>
