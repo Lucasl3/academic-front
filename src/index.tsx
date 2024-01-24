ffb05748745d063ff1be5111ab9cc7d313f2a6af
Lucas Lemos <llcf@ic.ufal.br>
2024-01-24 16:04:10 -0300

    feat: new providers

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom/client'
 
 import { ColorModeScript } from '@chakra-ui/react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -11,11 +13,25 @@ const container = document.getElementById('root')
 if (!container) throw new Error('Failed to find the root element')
 const root = ReactDOM.createRoot(container)
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 root.render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript />
-    <App />
-  </ChakraProvider>,
+  <QueryClientProvider client={queryClient}>
+    <GoogleOAuthProvider
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''}
+    >
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <App />
+      </ChakraProvider>
+    </GoogleOAuthProvider>
+  </QueryClientProvider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
