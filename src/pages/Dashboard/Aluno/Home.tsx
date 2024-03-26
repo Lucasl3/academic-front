37f1e3e66c03236add8df31ce9a3bf1750e34cac
Gabriela <leticia.lima@nees.ufal.br>
2024-03-26 06:48:46 -0300

    integra quantidade de solicitaçoes do aluno

--- src/pages/Dashboard/Aluno/Home.tsx
+++ src/pages/Dashboard/Aluno/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import {
   Stack,
@@ -11,9 +11,28 @@ import {
   Badge,
 } from '@chakra-ui/react'
 
+import { getSolicitationByUser } from '@/api/dashboard/solicitation/services'
 import NumberCaption from '@/components/DataDisplay/NumberCaption'
+import { AppContext } from '@/contexts/AppContext'
 
 const AlunoHome = () => {
+  const { user: loggedUser } = useContext(AppContext)
+  const [naoAtendidas, setNaoAtendidas] = useState(0)
+  const [emAndamento, setEmAndamento] = useState(0)
+  const [resolvidas, setResolvidas] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    getSolicitationByUser({ id: loggedUser.co_user }).then((res) => {
+      setTotal(res.length)
+      setNaoAtendidas(
+        res.filter((s: any) => s.coStatus === 0 || s.coStatus === 1).length,
+      )
+      setEmAndamento(res.filter((s: any) => s.coStatus === 2).length)
+      setResolvidas(res.filter((s: any) => s.coStatus === 3).length)
+    })
+  }, [])
+
   return (
     <Stack gap={5}>
       <Stack>
@@ -22,10 +41,18 @@ const AlunoHome = () => {
             Status das solicitações
           </Text>
           <HStack flexWrap="wrap" align="flex-start">
-            <NumberCaption number={7} caption="Não atendidas" flex={1} />
-            <NumberCaption number={15} caption="Em andamento" flex={1} />
-            <NumberCaption number={37} caption="Resolvidas" flex={1} />
-            <NumberCaption number={59} caption="Total" flex={1} />
+            <NumberCaption
+              number={naoAtendidas}
+              caption="Não atendidas"
+              flex={1}
+            />
+            <NumberCaption
+              number={emAndamento}
+              caption="Em andamento"
+              flex={1}
+            />
+            <NumberCaption number={resolvidas} caption="Resolvidas" flex={1} />
+            <NumberCaption number={total} caption="Total" flex={1} />
           </HStack>
         </Stack>
       </Stack>
