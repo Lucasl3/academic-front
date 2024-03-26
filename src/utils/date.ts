4542052c2e958d29fd580b45b07ef1166224fa91
Gabriela <leticia.lima@nees.ufal.br>
2024-03-25 22:04:16 -0300

    listagem de solicitacoes/demandas para secretaria

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import 'moment/locale/pt-br'
 
 export const formatDate = (
   timestamp: string,
@@ -12,4 +13,18 @@ export const formatDate = (
 
 export const getCurrentDate = (format: string = 'DD/MM/YYYY') => {
   return moment().format(format)
+}
+
+export const formatDateWithDayOfWeek = (
+  timestamp: string,
+  format: string = 'DD/MM/YYYY',
+) => {
+  if (!timestamp) return ''
+  moment.locale('pt-br')
+  const date = moment(timestamp)
+  const formattedDate = date.format(format)
+  const dayOfWeek = date.format('dddd')
+  const time = date.format('HH:mm')
+
+  return `${dayOfWeek.toUpperCase()}, ${formattedDate} às ${time}`
 }
