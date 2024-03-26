1de666e1a00373c69f69a50101855d2851cb11c5
Gabriela <leticia.lima@nees.ufal.br>
2024-03-26 07:36:02 -0300

    demandas novas e vistas recentemente

--- src/pages/Dashboard/Secretaria/Home.tsx
+++ src/pages/Dashboard/Secretaria/Home.tsx
@@ -11,18 +11,107 @@ import {
   Badge,
 } from '@chakra-ui/react'
 
+import { getForm } from '@/api/dashboard/forms/services'
 import { getSolicitations } from '@/api/dashboard/solicitation/services'
 import NumberCaption from '@/components/DataDisplay/NumberCaption'
 
 const SecretariaHome = () => {
   const [naoAtendidas, setNaoAtendidas] = useState(0)
+  const [demandasRecentes, setDemandasRecentes] = useState([
+    {
+      id: 0,
+      title: '',
+      data: '',
+    },
+  ])
+
+  const [demandasNovas, setDemandasNovas] = useState([
+    {
+      id: 0,
+      title: '',
+    },
+  ])
   const [emAndamento, setEmAndamento] = useState(0)
   const [resolvidas, setResolvidas] = useState(0)
   const [total, setTotal] = useState(0)
 
   useEffect(() => {
     getSolicitations().then((res) => {
-      console.log(res)
+      const novasDemandas = res
+        .filter((s: any) => s.coStatus === 0 || s.coStatus === 1)
+        .map((demanda: any) => {
+          return getForm({ id: demanda.coForm }).then((form) => {
+            return {
+              id: demanda.coSolicitation,
+              title: form.noForm,
+            }
+          })
+        })
+
+      Promise.all(novasDemandas)
+        .then((data) => {
+          setDemandasNovas(data)
+          // // Definir as demandas recentes com base nas datas mais recentes
+          // const demandasOrdenadasPorData = data.sort(
+          //   (a, b) => b.data.getTime() - a.data.getTime(),
+          // )
+          // const demandasRecentesAtualizadas = demandasOrdenadasPorData.slice(
+          //   0,
+          //   5,
+          // ) // Pegar as 5 demandas mais recentes
+          // console.log(demandasRecentesAtualizadas)
+          // setDemandasRecentes(demandasRecentesAtualizadas)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+      const recentes = res.map((demanda: any) => {
+        console.log(demanda)
+        return getForm({ id: demanda.coForm }).then((form) => {
+          return {
+            id: demanda.coSolicitation,
+            title: form.noForm,
+            data: demanda.dtUpdatedAt,
+          }
+        })
+      })
+      Promise.all(recentes)
+        .then((dadosRecentes) => {
+          // Definir as demandas recentes com base nas datas mais recentes
+          const demandasComData = dadosRecentes.filter(
+            (item) => item.data !== null,
+          )
+          console.log(demandasComData)
+
+          // Verificar se há demandas com data para evitar erro de ordenação
+          if (demandasComData.length > 0) {
+            // Ordenar as demandas com base nas datas mais recentes
+            const demandasOrdenadasPorData = demandasComData.sort((a, b) => {
+              const dateA =
+                a.data instanceof Date
+                  ? a.data.getTime()
+                  : Number.MIN_SAFE_INTEGER
+              const dateB =
+                b.data instanceof Date
+                  ? b.data.getTime()
+                  : Number.MIN_SAFE_INTEGER
+              return dateB - dateA
+            })
+
+            // Selecionar as 5 demandas mais recentes
+            const demandasRecentesAtualizadas = demandasOrdenadasPorData.slice(
+              0,
+              5,
+            )
+            setDemandasRecentes(demandasRecentesAtualizadas)
+          } else {
+            setDemandasRecentes([])
+          }
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+      console.log(recentes)
       setTotal(res.length)
       setNaoAtendidas(
         res.filter((s: any) => s.coStatus === 0 || s.coStatus === 1).length,
@@ -60,13 +149,13 @@ const SecretariaHome = () => {
           <Text fontSize="xl" fontWeight="semibold">
             Novas demandas
             <Badge fontSize="md" ml="2" colorScheme="green">
-              +4
+              +{naoAtendidas}
             </Badge>
           </Text>
           <Stack>
-            <Button>Demanda 1</Button>
-            <Button>Demanda 2</Button>
-            <Button>Demanda 3</Button>
+            {demandasNovas.map((demanda, index) => (
+              <Button key={index}>{demanda.title}</Button>
+            ))}
           </Stack>
         </Stack>
         <Stack boxShadow="lg" rounded="lg" p="6" bg="#FBFBFB" gap={5} flex={1}>
@@ -74,9 +163,9 @@ const SecretariaHome = () => {
             Demandas vistas recentemente
           </Text>
           <Stack>
-            <Button>Demanda 1</Button>
-            <Button>Demanda 2</Button>
-            <Button>Demanda 3</Button>
+            {demandasRecentes.map((demanda, index) => (
+              <Button key={index}>{demanda.title}</Button>
+            ))}
           </Stack>
         </Stack>
       </Stack>
